cd425b1b9b871dcc5bd2598d4cf1194f5f30a11c
ketorenoch <ketorenoch@gmail.com>
2023-03-27 16:11:15 +0000

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+function calSvings() {
+    let input_field = parseInt(document.getElementById('input_savings') .value);
+    let add_btn = document.querySelector('.add_btn');
+    let savings_btn = document.querySelector('.savings_btn');
+    //Adding savings start
+    add_btn.addEventListener('click';function() {
+        let input_field = parseInt(document.getElementById('input_savings').value);
+        alert(input_field);
+    });
+   // Add savings end
+
+
+}
